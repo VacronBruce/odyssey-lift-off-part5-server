a51e185e91b5fff883b3a493e1a33cac3315afa3
Bruce <homan311@gmail.com>
2022-06-28 07:44:17 +0800

    Change shema

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,10 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: () => {},
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
